1f89cf9293ba72bee85605fb3310a566c537e9fe
e <dev.jvsb@gmail.com>
2024-11-18 23:46:02 -0300

    refatoração

--- src/bikamp/Controllers/PontosControler.cs
+++ src/bikamp/Controllers/PontosControler.cs
@@ -1,9 +1,4 @@
-using System.Security.Cryptography.X509Certificates;
-using System.Transactions;
-using Dapper.Transaction;
-using Microsoft.AspNetCore.Mvc;
-
-namespace bikamp.Controllers;
+namespace Bikamp.Controllers;
 
 
 
@@ -13,7 +8,7 @@ namespace bikamp.Controllers;
 public class PontosController(IDbConnection conn) : ControllerBase
 {
     private readonly IDbConnection _conn = conn;
-    public record PontoInfo(int bicicletario, int ponto, string status_ponto, int? bicicleta, string status_bicicleta );
+    public record PontoInfo(int bicicletario, int ponto, string status_ponto, int? bicicleta, string status_bicicleta);
 
     [HttpGet("")]
     public async Task<ActionResult<List<PontoInfo>>> Get([FromQuery] int? id_bicicletario)
@@ -22,12 +17,12 @@ public class PontosController(IDbConnection conn) : ControllerBase
         var pontos = await tran.QueryAsync<PontoInfo>(@"SELECT 
                 ponto.bicicletario_id as bicicletario,
                 ponto.ponto_id as ponto,
-				ponto.status as status_ponto, 
-				bicicleta.bicicleta_id as bicicleta,
-				bicicleta.status as status_bicicleta
+                ponto.status as status_ponto, 
+                bicicleta.bicicleta_id as bicicleta,
+                bicicleta.status as status_bicicleta
             from ponto
             left join bicicleta on bicicleta.bicicleta_id = ponto.bicicleta_id
-			where @id_bicicletario is null or ponto.bicicletario_id = @id_bicicletario", new { id_bicicletario });
+            where @id_bicicletario is null or ponto.bicicletario_id = @id_bicicletario", new { id_bicicletario });
         tran.Commit();
         return Ok(pontos);
     }
@@ -40,12 +35,12 @@ public class PontosController(IDbConnection conn) : ControllerBase
         var result = await tran.QueryFirstOrDefaultAsync<PontoInfo>(@"SELECT 
                 ponto.bicicletario_id as bicicletario,
                 ponto.ponto_id as ponto,
-				ponto.status as status_ponto, 
-				bicicleta.bicicleta_id as bicicleta,
-				bicicleta.status as status_bicicleta
+                ponto.status as status_ponto, 
+                bicicleta.bicicleta_id as bicicleta,
+                bicicleta.status as status_bicicleta
             from ponto
             left join bicicleta on bicicleta.bicicleta_id = ponto.bicicleta_id
-			where  ponto.bicicletario_id = @bicicletario_id and ponto.ponto_id = @ponto_id", new { bicicletario_id, ponto_id });
+            where  ponto.bicicletario_id = @bicicletario_id and ponto.ponto_id = @ponto_id", new { bicicletario_id, ponto_id });
         tran.Commit();
         if (result == null)
             return NotFound();
@@ -58,7 +53,7 @@ public class PontosController(IDbConnection conn) : ControllerBase
         using IDbTransaction tran = _conn.BeginTransaction();
         await tran.ExecuteAsync(
             @"INSERT INTO ponto (bicicletario_id, ponto_id, status) 
-			VALUES  (@bicicletario_id, @ponto_id, @status)",
+            VALUES  (@bicicletario_id, @ponto_id, @status)",
             ponto
         );
         tran.Commit();
@@ -72,15 +67,15 @@ public class PontosController(IDbConnection conn) : ControllerBase
         using IDbTransaction tran = _conn.BeginTransaction();
         await tran.ExecuteAsync(
             @"UPDATE ponto 
-			SET 
-			  	status = @status 
-			WHERE bicicletario_id = @bicicletario_id and ponto_id = @ponto_id;",
+            SET 
+                  status = @status 
+            WHERE bicicletario_id = @bicicletario_id and ponto_id = @ponto_id;",
             ponto);
         tran.Commit();
         return Ok();
     }
 
- 
+
     public record AtualizacaoPontoBicicleta(int bicicletario_id, int ponto_id, int bicicleta_id);
 
     [HttpPut("bicicleta")]
@@ -94,13 +89,13 @@ public class PontosController(IDbConnection conn) : ControllerBase
                 exists(select * from ponto where bicicleta_id = @bicicleta_id)",
             request
         );
- 
-        if(jaPossuiBicicleta) 
+
+        if (jaPossuiBicicleta)
             return Conflict("Ponto já possui uma bicicleta registrada");
-        
+
         if (bicicletaEstaOutroPonto)
             return Conflict("Bicicleta já está em outro bicicletario ");
-        
+
         await tran.ExecuteAsync(
             @"UPDATE ponto 
             SET 
@@ -121,7 +116,7 @@ public class PontosController(IDbConnection conn) : ControllerBase
 
         int rows_affected = await tran.ExecuteAsync(
             @"DELETE FROM ponto 
-			WHERE bicicletario_id = @bicicletario_id and ponto_id = @ponto_id;",
+            WHERE bicicletario_id = @bicicletario_id and ponto_id = @ponto_id;",
             new { bicicletario_id, ponto_id });
         tran.Commit();
         if (rows_affected == 0)
--- src/bikamp/Models.cs
+++ src/bikamp/Models.cs
@@ -1,19 +1,19 @@
 public enum Cargo {
-	Supervisor = 1, 
-	Reparador = 2,
-	Administrador = 3, 
+    Supervisor = 1, 
+    Reparador = 2,
+    Administrador = 3, 
 
 }
 public record Mantenedor(int mantenedor_id, Cargo cargo, string nome);
 public record Bicicleta(int id, string status);
 public record BicicletaPonto(int id, string status, int? bicicletario, int? ponto);
 public record Emprestimo(
-	int ciclista_ra,
-	DateTime emprestimo_inicio,
-	DateTime? emprestimo_fim,
-	int? bicicletario_id_devolvido ,
-	int bicicletario_id_tirado,
-	int bicicleta_id,
-	bool cancelado
+    int ciclista_ra,
+    DateTime emprestimo_inicio,
+    DateTime? emprestimo_fim,
+    int? bicicletario_id_devolvido ,
+    int bicicletario_id_tirado,
+    int bicicleta_id,
+    bool cancelado
 );
   
