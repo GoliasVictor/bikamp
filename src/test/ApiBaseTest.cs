8fa51f301e57bc2ee2d7095f415491240ed2d750
e <dev.jvsb@gmail.com>
2024-11-20 00:14:09 -0300

    remove testes

--- src/test/ApiBaseTest.cs
+++
@@ -1,8 +0,0 @@
-namespace test;
-
-public abstract class ApiBaseTest {
-   public static HttpClient client = new HttpClient();
-   public ApiBaseTest(){
-        client.BaseAddress = new Uri(Config.UrlApi!);
-   }
-}
