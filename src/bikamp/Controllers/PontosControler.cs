b19132e140334183fa1ed534a98b0d29fa1dcea2
e <dev.jvsb@gmail.com>
2024-11-20 06:54:56 -0300

    conserta PontosController

--- src/bikamp/Controllers/PontosControler.cs
+++ src/bikamp/Controllers/PontosControler.cs
@@ -1,46 +1,77 @@
 namespace Bikamp.Controllers;
 
-[ApiController]
-[Route("cargo/")]
 
-public class CargoController(IDbConnection conn) : ControllerBase
+
+[ApiController]
+[Route("pontos/")]
+
+public class PontosController(IDbConnection conn) : ControllerBase
 {
     private readonly IDbConnection _conn = conn;
-    public record Cargo(int cargo_id, string nome);
-    [HttpGet()]
-    public async Task<ActionResult<List<Cargo>>> GetAll()
+    public record PontoInfo(int bicicletario, int ponto, string status_ponto, int? bicicleta, string status_bicicleta);
+
+    [HttpGet("")]
+    public async Task<ActionResult<List<PontoInfo>>> Get([FromQuery] int? id_bicicletario)
     {
         using IDbTransaction tran = _conn.BeginTransaction();
-
-        var result = await tran.QueryAsync<Cargo>(
-            @"select 
-                cargo_id,
-                cargo_nome as nome
-            from cargo"
-        );
-    
+        var pontos = await tran.QueryAsync<PontoInfo>(@"SELECT 
+                ponto.bicicletario_id as bicicletario,
+                ponto.ponto_id as ponto,
+                ponto.status as status_ponto, 
+                bicicleta.bicicleta_id as bicicleta,
+                bicicleta.status as status_bicicleta
+            from ponto
+            left join bicicleta on bicicleta.bicicleta_id = ponto.bicicleta_id
+            where @id_bicicletario is null or ponto.bicicletario_id = @id_bicicletario", new { id_bicicletario });
         tran.Commit();
-        return Ok(result);
+        return Ok(pontos);
     }
 
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Cargo>> Get(int id)
+    [HttpGet("{bicicletario_id}/{ponto_id}")]
+    public async Task<ActionResult<PontoInfo>> Get(int bicicletario_id, int ponto_id)
     {
         using IDbTransaction tran = _conn.BeginTransaction();
-
-        var result = await tran.QueryFirstOrDefaultAsync<Cargo>(
-            @"select 
-                cargo_id,
-                cargo_nome as nome
-            from cargo 
-            where cargo_id = @cargo_id",
-            new {
-                cargo_id = @id
-            }
-        );
-    
+        var result = await tran.QueryFirstOrDefaultAsync<PontoInfo>(@"SELECT 
+                ponto.bicicletario_id as bicicletario,
+                ponto.ponto_id as ponto,
+                ponto.status as status_ponto, 
+                bicicleta.bicicleta_id as bicicleta,
+                bicicleta.status as status_bicicleta
+            from ponto
+            left join bicicleta on bicicleta.bicicleta_id = ponto.bicicleta_id
+            where  ponto.bicicletario_id = @bicicletario_id and ponto.ponto_id = @ponto_id", new { bicicletario_id, ponto_id });
         tran.Commit();
+        if (result == null)
+            return NotFound();
         return Ok(result);
+    }
+    public record NovoPonto(int bicicletario_id, int ponto_id, string status);
+    [HttpPost("")]
+    public async Task<ActionResult> Post(NovoPonto ponto)
+    {
+        using IDbTransaction tran = _conn.BeginTransaction();
+        await tran.ExecuteAsync(
+            @"INSERT INTO ponto (bicicletario_id, ponto_id, status) 
+            VALUES  (@bicicletario_id, @ponto_id, @status)",
+            ponto
+        );
+        tran.Commit();
+        return Ok();
+    }
+    public record AtualizacaoPonto(int bicicletario_id, int ponto_id, string status);
+
+    [HttpPatch("")]
+    public async Task<ActionResult> Patch(AtualizacaoPonto ponto)
+    {
+        using IDbTransaction tran = _conn.BeginTransaction();
+        await tran.ExecuteAsync(
+            @"UPDATE ponto 
+            SET 
+                  status = @status 
+            WHERE bicicletario_id = @bicicletario_id and ponto_id = @ponto_id;",
+            ponto);
+        tran.Commit();
+        return Ok();
     }
 }
