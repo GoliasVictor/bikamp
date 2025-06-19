baa8f12b37060a8314797434ba503f457281d7ab
e <dev.jvsb@gmail.com>
2025-06-19 04:33:06 -0300

    feat: substitue modal por dialog

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,7 @@ import App from './App.tsx'
 import { AuthProvider } from './hooks/useAuth.tsx';
 import { ModalProvider } from './hooks/useModal.tsx';
 import ModalView  from './components/modalView.tsx'
+import { Toaster } from "@/components/ui/sonner"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
         <ModalProvider>
           <ModalView> 
             <App />
+            <Toaster />
           </ModalView>
         </ModalProvider>
       </AuthProvider>
